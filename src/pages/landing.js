af263531a1c9af33250594300457b2841f0346e9
Okuhle <okuhlecharlieman72@gmail.com>
2023-10-10 14:23:53 +0200

    correctly resized the image

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -15,15 +15,20 @@ function landing() {
           />
         </div>
         <div className="flex-auto py-6 sm:p-11 md:w-1/2">
-          <h1 className="text-4xl py-6 font-bold">
+          <h1 className="text-4xl pt-6 font-bold">
             Fed up with your{" "}
             <img
               src="https://i.ibb.co/4WVMrFz/61-UMo7-Th97-L-AC-SX425.jpg"
               alt="61-UMo7-Th97-L-AC-SX425"
               border="0"
-              width={20}
-            ></img>
-            ? Mark yourself as liberated.
+              width={30}
+              style={{ display: "inline-block", verticalAlign: "middle" }}
+            />{" "}
+            ?
+          </h1>
+
+          <h1 className="text-4xl pb-6 font-bold">
+            Mark yourself as liberated.
           </h1>
           <p className="text-slate-400">
             We specialize in social chatting services that connect people,
@@ -31,7 +36,6 @@ function landing() {
             you're looking for a platform to socialize, network, or simply have
             fun conversations, we've got you covered.
           </p>
-
           <div className="flex my-6">
             <SignInModal />
             <SignupModal />
